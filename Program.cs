0e6cd9a13e363377204e890d75543fee299955b1
Rajat <rajatprajapati09@gmail.com>
2023-04-01 00:10:53 +0530

    Line Comarison Problem program file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComarisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.ReadLine();
+        }
+    }
+}
